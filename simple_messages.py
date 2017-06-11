ad629073bb5d33e5db6625cfa5561bcf7d9270ec
v-taoyongguang <v-taoyongguang@xiaomi.com>
2017-06-11 17:23:19 +0800

    第二章练习

---
+++ simple_messages.py
@@ -0,0 +1,33 @@
+message="hello world"
+print(message)
+meassage="test every day"
+print(meassage)
+
+name="Alace Name"
+print(name.upper())
+print(name.lower())
+
+
+name = "Eric"
+print("hello "+name+",would you like to learn Python?")
+
+#test2
+name= "tOme"
+print(name.lower())
+print(name.upper())
+print(name.title())
+
+#test3
+name ="Albert Einstein"
+message='"A persion who never made a mistake never tried anything new"'
+print(name+" said: "+message)
+
+#test4
+name ="Albert Einstein"
+message=name+" said:"+' "A persion who never made a mistake never tried anything new"'
+print(message)
+
+#test5
+name =" Albert Einstein "
+message='"A persion who never made a mistake never tried anything new"'
+print(name.strip()+" said: \t\n"+message)
